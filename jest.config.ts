a3ad46c398900beecb67e00a92c5632248d1f5f4
Arpit <arpit.kingkrrish@gmail.com>
2025-06-29 15:13:33 +0200

    fix: babel compilation for test cases and css fix

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,16 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
 const config: JestConfigWithTsJest = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/js-with-babel", // use ts-jest preset that supports babel
+  transform: {
+    "^.+\\.(ts|tsx|js|jsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
   testEnvironment: "jsdom",
-
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
 
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
-
-  transform: {
-    "^.+\\.(ts|tsx)$": "babel-jest", // ts-jest needs babel to compile
   },
 };
 
